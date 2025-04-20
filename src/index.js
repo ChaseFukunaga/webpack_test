9a85b4e95bca8caa26dfaf3ea1d6b175b9819104
ChaseFukunaga <chasefuk@hawaii.edu>
2025-04-19 16:51:39 -1000

    Test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { greeting } from "./greeting.js";
+import './styles.sass';
 
 console.log(greeting);
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,51 @@
-// webpack.config.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+ // webpack.config.js
+ const path = require("path");
+ const HtmlWebpackPlugin = require("html-webpack-plugin");
+ 
+ 
+ module.exports = {
+   mode: "production",
+   entry: "./src/index.js",
+   output: {
+     filename: "bundle.js",
+     path: path.resolve(__dirname, "dist"),
+     clean: true,
+   },
+   module: {
+     rules: [
+    {
+        test: /\.ts$/, // 👈 Add this rule for TypeScript
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
 
-var ghpages = require('gh-pages');
-
-ghpages.publish('dist', function(err) {});
-
-module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+       {
+         test: /\.scss$/i,
+         use: ["style-loader", "css-loader", "sass-loader"],
+       },
+       {
+         test: /\.sass$/i,
+         use: [
+           "style-loader",
+           "css-loader",
+           {
+             loader: "sass-loader",
+             options: {
+               sassOptions: {
+                 indentedSyntax: true,
+               },
+             },
+           },
+         ],
+       },
+     ],
+   },
+   resolve: {
+    extensions: [".ts", ".js"], // 👈 Allow both .ts and .js imports
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-    }),
-  ]
-};
+   plugins: [
+     new HtmlWebpackPlugin({
+       template: "./src/index.html",
+     }),
+   ]
+ };
